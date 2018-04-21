1480f4aac371705e6cb2f8537ea474e5af76191c
miguelms19 <miguelms19@gmail.com>
2018-04-21 17:04:38 +0100

    changed settings

--- apocalypse/settings.py
+++ apocalypse/settings.py
@@ -130,7 +130,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-#STATIC_ROOT = "/home/cfedeploy/webapps/cfehome_static_root/"
 
 #MEDIA_URL = "/media/"
 
