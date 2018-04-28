3044a3e55565bdfc16448ad3a000592d408adb9f
ineffablelucky <kumar.lakshay1991@gmail.com>
2018-04-29 00:32:30 +0530

    each blog page working

--- apocalypse/views.py
+++ apocalypse/views.py
@@ -26,6 +26,7 @@ def milsim(request):
 def blog(request):
     all_blogs = Blog_Table.get_all()
     # print(all_blogs)
+
     sorted_blogs = []
     for blog in all_blogs:
         changed_date = datetime.strptime(blog['fields']['Date'],"%Y-%m-%d").strftime("%B %d, %Y")
@@ -33,12 +34,25 @@ def blog(request):
         sorted_blogs.append((blog['fields']['Date'],changed_date, blog['fields']['Heading'],
                             blog['fields']['Text'], blog['fields']['Head_Image'],
                             blog['id']))
+
     sorted_blogs.sort(reverse=True)
     print(sorted_blogs)
+
     return render(request, 'blog.html', {'sorted_blogs':sorted_blogs})
 
 def blogdetail(request):
-    return render(request, 'blogdetail.html')
+    return render(request, 'blogdetail2.html')
+
+def eachblog(request, blog_id):
+    blog_detail = Blog_Table.get(blog_id)
+    # print(blog_detail)
+
+    blog_date = Blog_Table.get(blog_id)['fields']['Date']
+    changed_date_format = datetime.strptime(blog_date,"%Y-%m-%d").strftime("%B %d, %Y")
+    # print(changed_date_format)
+
+    return render(request, 'blogdetail.html',{'blog_detail': blog_detail,
+                                                'changed_date_format': changed_date_format})
 
 def gallery(request):
 
