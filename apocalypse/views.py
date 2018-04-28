c6211d0257679e29e0e6ca61584834067c806708
ineffablelucky <kumar.lakshay1991@gmail.com>
2018-04-28 15:55:54 +0530

    blog attached to airtable.

--- apocalypse/views.py
+++ apocalypse/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from airtable import Airtable
+from datetime import datetime
 
 Gallery_Table = Airtable('appZJXumNCOFemo8r',  # base_id
               'Gallery Page', api_key='keyoOFryShWQQ1qGs') #apikey
+Blog_Table = Airtable('appf9kdwo9dS1DiM8',
+                'Blog', api_key='keyoOFryShWQQ1qGs')
 
 def home(request):
     return render(request, 'home.html')
@@ -21,7 +24,18 @@ def milsim(request):
     return render(request, 'milsim.html')
 
 def blog(request):
-    return render(request, 'blog.html')
+    all_blogs = Blog_Table.get_all()
+    # print(all_blogs)
+    sorted_blogs = []
+    for blog in all_blogs:
+        changed_date = datetime.strptime(blog['fields']['Date'],"%Y-%m-%d").strftime("%B %d, %Y")
+        # print(changed_date)
+        sorted_blogs.append((blog['fields']['Date'],changed_date, blog['fields']['Heading'],
+                            blog['fields']['Text'], blog['fields']['Head_Image'],
+                            blog['id']))
+    sorted_blogs.sort(reverse=True)
+    print(sorted_blogs)
+    return render(request, 'blog.html', {'sorted_blogs':sorted_blogs})
 
 def blogdetail(request):
     return render(request, 'blogdetail.html')
